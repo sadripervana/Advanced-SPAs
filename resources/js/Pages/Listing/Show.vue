ca859667e8d067b04eecc3c2f53008c691396ccb
sadri pervana <sadripervana@gmail.com>
2025-03-18 13:30:09 +0100

    Monthly Payment Calculator

--- resources/js/Pages/Listing/Show.vue
+++ resources/js/Pages/Listing/Show.vue
@@ -1,34 +1,73 @@
 <template>
     <div class="flex flex-col-reverse md:grid md:grid-cols-12 gap-4">
-      <Box class="md:col-span-7 flex items-center ">
-        <div class="w-full text-center font-medium text-gray-500">No images</div>
-      </Box>
-      <div class="md:col-span-5 flex flex-col gap-4">
-        <Box>
-          <template #header>
-            Basic info
-          </template>
-          <Price :price="listing.price" class="text-2xl font-bold" />
-          <ListingSpace :listing="listing" class="text-lg" />
-          <ListingAddress :listing="listing" class="text-gray-500" />
+        <Box class="md:col-span-7 flex items-center ">
+            <div class="w-full text-center font-medium text-gray-500">No images</div>
         </Box>
+        <div class="md:col-span-5 flex flex-col gap-4">
+            <Box>
+                <template #header>
+                    Basic info
+                </template>
+                <Price :price="listing.price" class="text-2xl font-bold" />
+                <ListingSpace :listing="listing" class="text-lg" />
+                <ListingAddress :listing="listing" class="text-gray-500" />
+            </Box>
 
-        <Box>
-          <template #header>
-            Offer
-          </template>
-          Make an offer
-        </Box>
-      </div>
+            <Box>
+                <template #header>
+                    Monthly Payment
+                </template>
+                <label class="label">Interest rate ({{ interestRate }})</label>
+                <input v-model.number="interestRate" type="range" min="0.1" max="30" step="0.1"
+                    class="w-full h-4 bg-gray-200 rounded-lg appearance-none cursor-pointer dark:bg-gray-700" />
+
+                <label class="label">Duration ({{ duration }})</label>
+                <input v-model.number="duration" type="range" min="3" max="35" step="1"
+                    class="w-full h-4 bg-gray-200 rounded-lg appearance-none cursor-pointer dark:bg-gray-700" />
+
+                <div class="text-gray-200 dark:text-gray-400 text-lg font-bold mt-4">
+                    <div class="text-gray-400 dark:text-gray-500">Monthly payment
+                    </div>
+                    <Price :price="monthlyPayment" class="text-white text-3xl font-bold" />
+                </div>
+                <div class="mt-2 text-gray-500">
+                    <div class="flex justify-between">
+                        <div>Total paid</div>
+                        <div>
+                            <Price :price="totalPaid" class="font-medium" />
+                        </div>
+                    </div>
+                    <div class="flex justify-between">
+                        <div>Principal paid</div>
+                        <div>
+                            <Price :price="listing.price" class="font-medium" />
+                        </div>
+                    </div>
+                    <div class="flex justify-between">
+                        <div>Interest paid</div>
+                        <div>
+                            <Price :price="totalInterest" class="font-medium" />
+                        </div>
+                    </div>
+                </div>
+            </Box>
+        </div>
     </div>
-  </template>
+</template>
 <script setup>
 import ListingAddress from '@/Components/ListingAddress.vue';
 import ListingSpace from '@/Components/ListingSpace.vue';
 import Price from '@/Components/Price.vue';
 import Box from '@/Components/UI/Box.vue';
+import { ref } from 'vue';
 
-defineProps({
+import { useMonthlyPayment } from '@/Composables/useMonthlyPayment';
+
+const interestRate = ref(2.5);
+const duration = ref(25);
+
+const props = defineProps({
     listing: Object,
 })
+const { monthlyPayment, totalPaid, totalInterest } = useMonthlyPayment(props.listing.price, interestRate, duration)
 </script>
